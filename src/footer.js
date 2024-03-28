cc4c226cc09d3c02f7927e48220030ff83babf31
ysandeep832 <yalagirisandeep832@gmail.com>
2024-03-28 12:46:04 +0530

    firstFile

---
+++ src/footer.js
@@ -0,0 +1,41 @@
+
+
+
+function Footers(){
+
+    return(
+
+       
+        
+        <div class="parentDiv">
+        <div class="div1">
+        <div><h4>About TravelPro</h4></div>
+        <div><a href="##">About Us</a></div>
+        <div><a href="##">Contact Us</a></div>
+        </div>
+         <div class="div1">
+            <div><h4>Product</h4></div>
+            <div><a href="##">API</a></div>
+            <div><a href="##">Features</a></div>
+            </div>
+            <div class="div1">
+                <div ><h4>Legal</h4></div>
+                <div><a href ="##">terms and Conditions</a></div>
+                <div><a href="##">Copy Right</a></div>
+                </div>
+
+                <div class="div1">
+                    <div><h4>Contact</h4></div>
+                    <div><a href="##">contact@gmail.com</a></div>
+                    <div class="number">Ph.no:+6310000100</div>
+                    </div> 
+                  
+ 
+
+        </div>
+        
+       
+    )
+
+}
+export default Footers
